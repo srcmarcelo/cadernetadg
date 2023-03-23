92c0d4318cc0e093292e068bdc3f589dc3bbf8a0
Marcelo Cristian <marcelo.cristian.969@gmail.com>
2023-03-23 20:08:27 -0300

    updating next version

--- componets/CPCards/AlertsCard/index.jsx
+++ componets/CPCards/AlertsCard/index.jsx
@@ -47,7 +47,7 @@ export default function AlertsCard() {
 
   const warningLabels = {
     debtorDependency: {
-      title: 'Dependência de devedores',
+      title: 'Dependência de devedores [teste]',
       description:
         'Isso significa que está dependendo de um ou mais devedores para conseguir quitar suas dívidas.',
       label: 'Quanto ainda receberá',
